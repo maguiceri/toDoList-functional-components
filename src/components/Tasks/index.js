cf452f68090360a6ddf03275a646aa50c0535d36
magui <magui@Maguis-MacBook-Pro.local>
2021-08-23 17:00:42 -0300

    tasks

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -9,14 +9,14 @@ function Tasks(){
         setInputValue(e.target.value)
     }
     function handleClick(){ 
-        if(inputValue != ""){
-            setTasks([...tasks, inputValue]) //cloname el array tasks y agregale lo que tiene inputValue//setTasks(tasks.push(inputValue)) TASK.PUSH DEVUELVE LA CANTIDAD DE ELEMENTOS QUE TIENE UN ARRAY DESPUES DE INSERTARR
+        if(inputValue != ""){ //CONDICIONAL PARA QUE NO SE AGREGUE UN ITEM SI EL INPUT ESTA VACIO
+            setTasks([...tasks, inputValue]) //cloname el array tasks y agregale lo que tiene inputValue//setTasks(tasks.push(inputValue)) TASK.PUSH DEVUELVE LA CANTIDAD DE ELEMENTOS QUE TIENE UN ARRAY DESPUES DE EL ULTIMO QUE AGREGAMOS.
             setInputValue("")
         }
     }
     return(
         <div>
-            <input type="text" onChange={handleChange} value={inputValue}></input>
+            <input type="text" onChange={handleChange} value={inputValue}></input> 
             <button onClick={handleClick}>Agregar</button>
             <ul>
                 {tasks.map((name, key)=> {
